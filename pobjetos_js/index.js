4070aa9dfdfccbc36c12bc28cc36711688f3742e
JvPelai <61389657+JvPelai@users.noreply.github.com>
2020-10-10 17:14:46 -0300

    modularização e novos métodos

--- pobjetos_js/index.js
+++ pobjetos_js/index.js
@@ -1,24 +1,30 @@
-class Cliente {
-    nome;
-    cpf;
-    rg;
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
-class contaCorrente {
-    agencia;
-    saldo;
-    sacar(valor){
-        if(this.saldo >= valor){
-            this.saldo -= valor;
-        }
-    }
-}
+const cliente1 = new Cliente();
 
-contaCorrenteRicardo = new contaCorrente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
 
-contaCorrenteRicardo.saldo = 100;
+const cliente2 = new Cliente();
+
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+const contaCorrenteRicardo = new ContaCorrente();
+
 contaCorrenteRicardo.agencia = 1001;
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.sacar(50);
-console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
+const conta2 = new ContaCorrente();
+
+conta2.cliente = cliente2;
+conta2.agencia = 102;
+
+
+console.log(contaCorrenteRicardo);
+console.log(conta2);
+
+contaCorrenteRicardo.transferir(200,conta2);
+console.log(conta2);
